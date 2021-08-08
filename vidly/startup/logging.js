4eb1a0910c4e878be4c5c9a653b25e31cd71c7e9
amititme68 <amititme68@gmail.com>
2021-08-08 21:32:47 +0530

    prepared the app to deploy to heroku

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -1,6 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
-require('winston-mongodb');
+//require('winston-mongodb');
 
 module.exports = function(){
   
@@ -13,8 +13,8 @@ winston.handleExceptions(
 });
 
 winston.add(winston.transports.File, {filename: 'logfile.log'});
-winston.add(winston.transports.MongoDB, {
-  db:'mongodb://localhost/vidly',
-  level:'error',  
-}); 
+// winston.add(winston.transports.MongoDB, {
+//   db:'mongodb://localhost/vidly',
+//   level:'error',  
+// }); 
 }
